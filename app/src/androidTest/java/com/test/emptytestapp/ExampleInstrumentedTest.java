babb6b98e0be00fc324c81508ed998098433f802
Jay Cunnington <jay.cunnington@ctt.co.uk>
2020-03-20 16:37:09 +0000

    no message

--- app/src/androidTest/java/com/test/emptytestapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/test/emptytestapp/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package com.test.emptytestapp;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
