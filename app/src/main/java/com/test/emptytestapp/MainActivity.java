0c1193289963c83d3a3cc6f26bfb2d3c5d6bd0fe
Jay Cunnington <jay.cunnington@ctt.co.uk>
2020-03-25 09:18:34 +0000

    Adding cloud yaml

--- app/src/main/java/com/test/emptytestapp/MainActivity.java
+++ app/src/main/java/com/test/emptytestapp/MainActivity.java
@@ -11,6 +11,7 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
